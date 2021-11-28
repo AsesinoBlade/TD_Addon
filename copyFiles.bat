de664f530fa9739df45f58a18714595ac3ff2e5b
George.Saeed <gsaeed@yahoo.com>
2021-11-28 15:25:52 +0000

    Created xcopy bat

---
+++ copyFiles.bat
@@ -0,0 +1,4 @@
+xcopy /y /i /d /e "C:\Users\gsaee\Projects\TD_Addon\01 TR BSA\*.*" "C:\Games\OpenMW\MO2\mods\Tamriel Data (HD)"
+xcopy /y /i /d /e "C:\Users\gsaee\Projects\TD_Addon\02 TR BSA - HD Override\*.*" "C:\Games\OpenMW\MO2\mods\Tamriel Data (HD)"
+xcopy /y /i /d /e "C:\Users\gsaee\Projects\TD_Addon\03 PT BSA\*.*" "C:\Games\OpenMW\MO2\mods\Tamriel Data (HD)"
+xcopy /y /i /d /e "C:\Users\gsaee\Projects\TD_Addon\04 PT BSA - HD Override\*.*" "C:\Games\OpenMW\MO2\mods\Tamriel Data (HD)"
